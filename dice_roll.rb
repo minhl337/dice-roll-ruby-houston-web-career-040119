2f6ebadfa0b7d7e5566daf0ed56e802243681d42
minh nguyen <imnotminh@gmail.com>
2019-03-17 19:36:58 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,7 @@
 # Create method `roll` that returns a random number between 1 and 6
 
 # Feel free to google "how to generate a random number in ruby"
-
+$goose=[1,2,3,4,5,6]
 def roll
-  # code goes here
+  return goose[rand(6)]
 end
